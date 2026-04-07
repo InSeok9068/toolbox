5f20317e2d2b15d16ede6455bd9e27c5d7cf6335
Lee In Seok <dlstjr9068@gmail.com>
2026-04-07 15:19:24 +0900

    코드 통일성

--- scripts/powershell/fzf-rg.ps1
+++ scripts/powershell/fzf-rg.ps1
@@ -28,7 +28,7 @@ $rgPrefixParts = @(
 $rgPrefix = $rgPrefixParts -join ' '
 $startReload = "start:reload:$rgPrefix '' || cd ."
 $changeReload = "change:reload:$rgPrefix {q} || cd ."
-$previewCommand = 'bat --style=numbers --color=always --highlight-line {2} -- {1}'
+$previewCommand = 'bat --color=always --style=numbers --line-range=:500 --highlight-line {2} -- {1}'
 
 fzf `
     --ansi `
